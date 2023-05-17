3d5c1942b4de739225a959c2851d1c6e7f203577
kpriyanka0704 <priyankakardile10@gmail.com>
2023-05-17 09:21:36 +0530

    Add project files.

---
+++ ProjectWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectWebApplication.Models;
+using System.Diagnostics;
+
+namespace ProjectWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<string> colors = new List<string>()
+            {
+                "Red","Orange","Yellow","Purple","Pink","Green"
+            };
+            ViewData["Colors"] = colors;
+            ViewData["message"] = "Hello from Controller";
+
+            //using ViewBag
+            ViewBag.Colors = colors;
+            return View();
+           
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProjectWebApplication/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectWebApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult ProductList()
+        {
+            return View();
+        }
+    }
+}
+
